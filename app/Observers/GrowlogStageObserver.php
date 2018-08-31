da26176b0c5c255e2d2207358e9ae2b46ad3528e
boryeira <jmanuel.jorquera@gmail.com>
2018-08-30 22:49:47 -0300

    OBSERVER

--- app/Observers/GrowlogStageObserver.php
+++ app/Observers/GrowlogStageObserver.php
@@ -8,25 +8,40 @@ use Session;
 class GrowlogStageObserver
 {
       public function updating(GrowlogStage $growlogstage){
-        $prev = $growlogstage->prevStage();
 
-        $prev_start = new Carbon($prev->stage_start);
+        //VERIFICACION QUE LA ETAPA A ACTUALIZAR CUMPLA LAS REGLAS
         $actual_start = new Carbon($growlogstage->stage_start);
 
-
-
+        //PREVIA
+        $prev = $growlogstage->prevStage();
         while($prev!=null){
           $prev_start = new Carbon($prev->stage_start);
-          $actual_start = new Carbon($growlogstage->stage_start);
-          if($actual_start->lt($prev_start)){
+          if(($actual_start->lt($prev_start))&&($prev->stage_start!=null) ){
             session::flash('warning','es mas chico');
             return false;
-
           }
-          else{
+          else {
             $prev = $prev->prevStage();
           }
         }
+        //FIN PREVIA
+        //NEXT
+        $next = $growlogstage->nextStage();
+
+        while($next!=null){
+          $next_start = new Carbon($next->stage_start);
+
+          if(($actual_start->gt($next_start))&&($next->stage_start!=null) ){
+            session::flash('warning','es mas grande');
+            return false;
+
+          }
+          else {
+            $next = $next->nextStage();
+          }
+        }
+        //FIN NEXT
+
         session::flash('success','todo bien');
 
       }
